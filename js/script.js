174ba9b34e90adbe0856e9d456ac325d10103435
Tu Vo <tuvo1106@gmail.com>
2019-07-06 18:36:57 -0700

    Add nav menu and preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+$(window).on("load", function() {
+	$(".loader .inner").fadeOut(300, function() {
+		$(".loader").fadeOut(600);
+	});
+})
+
 $(document).ready(function() {
 
 	$('#slides').superslides({
@@ -62,6 +68,28 @@ $(document).ready(function() {
 		}
 	});
 
+	$("#navigation li a").click(function(e) {
+		e.preventDefault();
 
+		var targetElement = $(this).attr("href");
+		var targetPosition = $(targetElement).offset().top;
+		$("html, body").animate({ scrollTop: targetPosition - 50}, "slow")
+	});
+
+	const nav = $("#navigation");
+	const navTop = nav.offset().top;
+
+	$(window).on("scroll", stickyNavigation);
+
+	function stickyNavigation() {
+		var body = $("body");
+		if ($(window).scrollTop() >= navTop) {
+			body.css("padding-top", nav.outerHeight() + "px")
+			body.addClass("fixedNav")
+		} else {
+			body.css("padding-top", 0)
+			body.removeClass("fixedNav")
+		}
+	}
 
 })
