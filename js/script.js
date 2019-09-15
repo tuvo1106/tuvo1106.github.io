be082f73c76d2af6747ba57734abe22630aa3f5a
Tu Vo <tuvo1106@gmail.com>
2019-09-15 10:26:01 -0700

    Reworked landing page and reduce JS clutter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(window).on("load", function() {
-	$(".loader .inner").fadeOut(300, function() {
-		$(".loader").fadeOut(600);
+	$(".loader .inner").fadeOut(100, function() {
+		$(".loader").fadeOut(250);
 	});
 })
 
@@ -20,77 +20,77 @@ $(document).ready(function() {
 		showCursor: false
 	});
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	       	938:{
-	            items:4
-	        }
-	    }
-	});
+	// $('.owl-carousel').owlCarousel({
+	//     loop:true,
+	//     items: 4,
+	//     responsive:{
+	//         0:{
+	//             items:1
+	//         },
+	//         480:{
+	//             items:2
+	//         },
+	//         768:{
+	//             items:3
+	//         },
+	//        	938:{
+	//             items:4
+	//         }
+	//     }
+	// });
 
-	var skillsTopOffset = $(".skillsSection").offset().top;
-	var statsTopOffset = $(".statsSection").offset().top;
-	var countUpFinished = false;
+	// var skillsTopOffset = $(".skillsSection").offset().top;
+	// var statsTopOffset = $(".statsSection").offset().top;
+	// var countUpFinished = false;
 
-	$(window).scroll(function() {
-		if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-			$('.chart').easyPieChart({
-		    	easing: 'easeInOut',
-		    	barColor: 'white',
-		    	trackColor: false,
-		    	scaleColor: false,
-		    	lineWidth: 4,
-		    	size: 152,
-		    	onStep: function(from, to, percent) {
-		    		$(this.el).find('.percent').text(Math.round(percent));
-		    	}
-    		});
-		}
+	// $(window).scroll(function() {
+	// 	if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+	// 		$('.chart').easyPieChart({
+	// 	    	easing: 'easeInOut',
+	// 	    	barColor: 'white',
+	// 	    	trackColor: false,
+	// 	    	scaleColor: false,
+	// 	    	lineWidth: 4,
+	// 	    	size: 152,
+	// 	    	onStep: function(from, to, percent) {
+	// 	    		$(this.el).find('.percent').text(Math.round(percent));
+	// 	    	}
+ //    		});
+	// 	}
 
-		if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-			$(".counter").each(function() {
-				var element = $(this);
-				var endVal = parseInt(element.text());
-				element.countup(endVal);
-			})
-			countUpFinished = true;
-		}
-	});
+	// 	if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+	// 		$(".counter").each(function() {
+	// 			var element = $(this);
+	// 			var endVal = parseInt(element.text());
+	// 			element.countup(endVal);
+	// 		})
+	// 		countUpFinished = true;
+	// 	}
+	// });
 
-	$("#navigation li a").click(function(e) {
-		e.preventDefault();
+	// $("#navigation li a").click(function(e) {
+	// 	e.preventDefault();
 
-		var targetElement = $(this).attr("href");
-		var targetPosition = $(targetElement).offset().top;
-		$("html, body").animate({ scrollTop: targetPosition - 50}, "slow")
-	});
+	// 	var targetElement = $(this).attr("href");
+	// 	var targetPosition = $(targetElement).offset().top;
+	// 	$("html, body").animate({ scrollTop: targetPosition - 50}, "slow")
+	// });
 
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
+	// const nav = $("#navigation");
+	// const navTop = nav.offset().top;
 
-	$(window).on("scroll", stickyNavigation);
+	// $(window).on("scroll", stickyNavigation);
 
-	function stickyNavigation() {
-		var body = $("body");
-		if ($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px")
-			body.addClass("fixedNav")
-		} else {
-			body.css("padding-top", 0)
-			body.removeClass("fixedNav")
-		}
-	}
+	// function stickyNavigation() {
+	// 	var body = $("body");
+	// 	if ($(window).scrollTop() >= navTop) {
+	// 		body.css("padding-top", nav.outerHeight() + "px")
+	// 		body.addClass("fixedNav")
+	// 	} else {
+	// 		body.css("padding-top", 0)
+	// 		body.removeClass("fixedNav")
+	// 	}
+	// }
 
 	$("[data-fancybox]").fancybox();
 
